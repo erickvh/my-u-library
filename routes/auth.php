6d76f6daccc2cf169e71b4201f3d9d558716eeaf
Erick Ventura <erick94111@gmail.com>
2022-09-15 10:54:40 -0600

    change middleware for register route permission instead role

--- routes/auth.php
+++ routes/auth.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::post('register', [AuthController::class, 'register'])->middleware(['auth:sanctum', 'role:librarian']);
+Route::post('register', [AuthController::class, 'register'])->middleware(['auth:sanctum', 'permission:user.create']);
 Route::post('login', [AuthController::class, 'login']);
 
 
